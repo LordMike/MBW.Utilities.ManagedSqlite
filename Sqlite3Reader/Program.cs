45c2a93ae9c2cbce07be697bb4eff7d6a229cf3e
Michael Bisbjerg <michael@mbwarez.dk>
2017-08-02 00:25:20 +0200

    Update demo app

--- Sqlite3Reader/Program.cs
+++ Sqlite3Reader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Sqlite3RoLib;
 using Sqlite3RoLib.Tables;
@@ -12,14 +13,35 @@ namespace Sqlite3Reader
             using (FileStream fs = File.OpenRead("Db3.db"))
             using (Sqlite3Database db = new Sqlite3Database(fs))
             {
-                var tables = db.GetTables();
+                IEnumerable<Sqlite3SchemaRow> tables = db.GetTables();
 
                 foreach (Sqlite3SchemaRow table in tables)
                 {
-                    Console.WriteLine(table.Name + ", " + table.TableName + ": " + table.Sql + "; " + table.RootPage);
+                    Console.WriteLine($"{table.Type} \"{table.Name}\", [{table.TableName}] (RP: {table.RootPage}): {table.Sql}; ");
+
+                    if (table.Type == "table")
+                    {
+                        Sqlite3Table tableData = db.GetTable(table.Name);
+
+                        foreach (Sqlite3Row row in tableData.EnumerateRows())
+                        {
+                            Console.Write(row.RowId);
+
+                            foreach (object obj in row.ColumnData)
+                            {
+                                Console.Write(" | ");
+
+                                if (obj == null)
+                                    Console.Write(" <null> ");
+                                else
+                                    Console.Write(obj);
+                            }
+
+                            Console.WriteLine();
+                        }
+                    }
                 }
             }
-
         }
     }
 }
