a0c2f37f97e6eb1e805ce000dd21c7786c232de7
Michael Bisbjerg <michael@mbwarez.dk>
2022-08-17 21:48:36 +0200

    Add docs

--- src/MBW.Utilities.ManagedSqlite.Core/Sqlite3Settings.cs
+++ src/MBW.Utilities.ManagedSqlite.Core/Sqlite3Settings.cs
@@ -4,6 +4,11 @@ namespace MBW.Utilities.ManagedSqlite.Core
 {
     public class Sqlite3Settings
     {
+        /// <summary>
+        /// Should the sqlite database have an invalid text encoding, it can be overridden here.
+        ///
+        /// This is a dangerous setting, as an incorrect encoding will provide garbage data.
+        /// </summary>
         public SqliteEncoding? FallbackEncoding { get; set; }
     }
 }
